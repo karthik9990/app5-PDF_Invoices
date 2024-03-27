365fdda33eb2f636540b541dea643bf68dd989b9
karthik9990 <karthik.tadakapalli@gmail.com>
2024-03-27 11:59:42 -0600

    Day 26: we invoice added Date

--- main.py
+++ main.py
@@ -9,12 +9,19 @@ print(filepaths)
 
 for filepath in filepaths:
     df = pd.read_excel(filepath, sheet_name="Sheet 1")
+
     pdf = FPDF(orientation="P", unit="mm", format="A4")
     pdf.add_page()
+
     filename = Path(filepath).stem
     invoice_nr = filename.split("-")[0]
     pdf.set_font(family="Arial", size=16, style="B")
-    pdf.cell(w=50, h=8, txt=f"Invoice Number: {invoice_nr}")
+    pdf.cell(w=50, h=8, txt=f"Invoice Number: {invoice_nr}", ln=1)
+
+    date = filename.split("-")[1]
+    pdf.set_font(family="Arial", size=16, style="B")
+    pdf.cell(w=50, h=8, txt=f"Date: {date}")
+
     pdf.output(f"PDFs/{filename}.pdf")
 
 
